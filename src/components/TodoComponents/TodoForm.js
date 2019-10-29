37104bb1b142968ad745087b8e61e4bdce6d3aef
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-28 19:43:48 -0500

    created components and importing TodoForm to App

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todo: ''
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <label htmlFor='form'></label>
+                        <input 
+                        name='todo'
+                        id='todo'
+                        type='text'
+                        placeholder='Add New Todo'
+                        value={this.state.todo}
+                        />
+                </form>
+            </div>
+        )
+    }
+}
